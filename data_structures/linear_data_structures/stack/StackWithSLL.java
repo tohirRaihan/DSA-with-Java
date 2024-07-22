0653e339c3f4dc7bddf08bdc474485adf9cd171b
Tohir Raihan <tohir.raihan@gmail.com>
2024-07-22 22:26:40 +0600

    :pencil2: Add: StackWithSLL

---
+++ data_structures/linear_data_structures/stack/StackWithSLL.java
@@ -0,0 +1,58 @@
+package data_structures.linear_data_structures.stack;
+
+import data_structures.linear_data_structures.linked_list.SinglyLinkedList;
+
+public class StackWithSLL {
+  SinglyLinkedList linkedList;
+
+  public StackWithSLL() {
+    linkedList = new SinglyLinkedList();
+  }
+
+  // Push method
+  public void push(int value) {
+    linkedList.insertInLinkedList(value, 0);
+    System.out.println("Inserted " + value + " in Stack");
+  }
+
+  // isEmpty
+  public boolean isEmpty() {
+    return linkedList.isEmpty();
+  }
+
+  // Pop method
+  public int pop() {
+    int result = -1;
+    if (isEmpty()) {
+      System.out.println("The Stack is Empty!");
+    } else {
+      result = linkedList.getValue(0);
+      linkedList.deletionOfNode(0);
+    }
+    return result;
+  }
+
+  // Peek Method
+  public int peek() {
+    if (isEmpty()) {
+      System.out.println("The Stack is Empty!");
+      return -1;
+    } else {
+      return linkedList.getValue(0);
+    }
+  }
+
+  // Delete Method
+  public void deleteStack() {
+    linkedList.deleteSLL();
+    System.out.println("The Stack is deleted");
+  }
+
+  public static void main(String[] args) {
+    StackWithSLL newStack = new StackWithSLL();
+    newStack.push(1);
+    newStack.push(2);
+    newStack.push(3);
+    newStack.deleteStack();
+  }
+}
