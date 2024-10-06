9d9bba1d55e1e7199f4739d56504b0a8898722cc
Tohir Raihan <tohir.raihan@gmail.com>
2024-10-06 22:45:23 +0600

    :pencil2: Add: BubbleSort class added

---
+++ algorithms/sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+package algorithms.sorting;
+
+public class BubbleSort {
+
+  public void bubbleSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  }
+
+  public void printArray(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+}
