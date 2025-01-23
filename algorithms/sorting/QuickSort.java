0b8525cf1824a3d49fe938c1f0c2780bfc467972
Tohir Raihan <tohir.raihan@gmail.com>
2025-01-24 00:12:21 +0600

    :pencil2: Add: QuickSort algorithm added

---
+++ algorithms/sorting/QuickSort.java
@@ -0,0 +1,31 @@
+package algorithms.sorting;
+
+public class QuickSort {
+  static int partition(int[] array, int start, int end) {
+    int pivot = end;
+    int i = start - 1;
+    for (int j = start; j <= end; j++) {
+      if (array[j] <= array[pivot]) {
+        i++;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+    return i;
+  }
+
+  public static void quickSort(int[] array, int start, int end) {
+    if (start < end) {
+      int pivot = partition(array, start, end);
+      quickSort(array, start, pivot - 1);
+      quickSort(array, pivot + 1, end);
+    }
+  }
+
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + "  ");
+    }
+  }
+}
